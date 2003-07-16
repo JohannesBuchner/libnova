f087513972ba0aeef65252dcdfb1f54142aa781e
l_girdwood <l_girdwood@a9329980-0c59-416b-a420-72dd107c4633>
2003-07-16 21:21:14 +0000

    initial revision

---
+++ examples/comet.c
@@ -0,0 +1,120 @@
+/*
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
+
+Copyright (C) 2003 Liam Girdwood <liam@nova-ioe.org>
+
+
+A simple example showing some comet calculations.
+
+Comet Enckle
+
+*/
+
+#include <stdio.h>
+#include "libnova.h"
+
+void print_date (char * title, struct ln_date* date)
+{
+	printf ("\n%s\n",title);
+	printf (" Year    : %d\n", date->years);
+	printf (" Month   : %d\n", date->months);
+	printf (" Day     : %d\n", date->days);
+	printf (" Hours   : %d\n", date->hours);
+	printf (" Minutes : %d\n", date->minutes);
+	printf (" Seconds : %f\n", date->seconds);
+}
+
+int main (int argc, char * argv[])
+{
+	struct ln_helio_posn pos;
+	struct lnh_equ_posn hequ;
+	struct ln_equ_posn equ;
+	struct ln_rst_time rst;
+	struct ln_date rise, set, transit, epoch_date;
+	struct ln_lnlat_posn observer;
+	struct ln_ell_orbit orbit;
+	struct ln_rect_posn posn;
+	double JD, e_JD;
+	double au, E, v, V, r, l, dist;
+	
+	/* observers location (Edinburgh), used to calc rst */
+	observer.lat = 55.92;
+	observer.lng = 3.18;
+	
+	/* get Julian day from local time */
+	JD = get_julian_from_sys();	
+	printf ("JD %f\n", JD);
+	
+	/* calc epoch JD */
+	epoch_date.years = 1990;
+	epoch_date.months = 10;
+	epoch_date.days = 28;
+	epoch_date.hours = 12;
+	epoch_date.minutes = 30;
+	epoch_date.seconds = 0;
+	e_JD = get_julian_day (&epoch_date);
+	
+	/* Enckle orbital elements */
+	orbit.JD = e_JD;
+	orbit.a = 2.2091404;
+	orbit.e = 0.8502196;
+	orbit.i = 11.94525;
+	orbit.omega = 334.75006;
+	orbit.w = 186.23352;
+	orbit.n = 0;
+	
+	E = solve_kepler (0.1, 5.0);
+	printf("(Equation of kepler) E when e is 0.1 and M is 5.0  %f\n ", E);
+	
+	v = get_ell_true_anomaly (0.1, E);
+	printf("(True Anomaly) v when e is 0.1 and E is 5.5545  %f\n ", v);
+	
+	r = get_ell_radius_vector (0.5, 0.1, E);
+	printf ("(Radius Vector) r when v is , e is 0.1 and E is 5.5545  %f\n ", r);
+	
+	get_ell_geo_rect_posn (&orbit, JD, &posn);
+	printf ("(Geocentric Rect Coords X) for comet Enckle  %f\n", posn.X);
+	printf ("(Geocentric Rect Coords Y) for comet Enckle  %f\n", posn.Y);
+	printf ("(Geocentric Rect Coords Z) for comet Enckle  %f\n", posn.Z);
+	
+	get_ell_helio_rect_posn (&orbit, JD, &posn);
+	printf ("(Heliocentric Rect Coords X) for comet Enckle  %f\n ", posn.X);
+	printf ("(Heliocentric Rect Coords Y) for comet Enckle  %f\n ", posn.Y);
+	printf ("(Heliocentric Rect Coords Z) for comet Enckle  %f\n ", posn.Z);
+	
+	get_ell_body_equ_coords (JD, &orbit, &equ);
+	printf ("(RA) for comet Enckle  %f\n ", equ.ra);
+	printf ("(Dec) for comet Enckle  %f\n ", equ.dec);
+	
+	l = get_ell_orbit_len (&orbit);
+	printf ("(Orbit Length) for comet Enckle in AU  %f\n ", l);
+	
+	V = get_ell_orbit_pvel (&orbit);
+	printf ("(Orbit Perihelion Vel) for comet Enckle in kms  %f\n ", V);
+	
+	V = get_ell_orbit_avel (&orbit);
+	printf ("(Orbit Aphelion Vel) for comet Enckle in kms  %f\n ", V);
+	
+	V = get_ell_orbit_vel (JD, &orbit);
+	printf ("(Orbit Vel JD) for comet Enckle in kms  %f\n ", V);
+	
+	dist = get_ell_body_solar_dist (JD, &orbit);
+	printf ("(Body Solar Dist) for comet Enckle in AU  %f\n ", dist);
+	
+	dist = get_ell_body_earth_dist (JD, &orbit);
+	printf ("(Body Earth Dist) for comet Enckle in AU  %f\n ", dist);
+	
+	return 0;
+}
