32552694bb5fb0b3abdbf9982aa400621323cfc7
l_girdwood <l_girdwood@a9329980-0c59-416b-a420-72dd107c4633>
2003-07-17 19:54:19 +0000

    initial revision

---
+++ examples/sun.c
@@ -0,0 +1,78 @@
+/*
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
+
+Copyright (C) 2003 Liam Girdwood <liam@nova-ioe.org>
+
+
+A simple example showing some solar calculations.
+
+*/
+
+#include <stdio.h>
+#include "libnova.h"
+
+void print_date (char * title, struct ln_date* date)
+{
+	printf ("\n%s\n",title);
+	printf (" Year    : %d\n", date->years);
+	printf (" Month   : %d\n", date->months);
+	printf (" Day     : %d\n", date->days);
+	printf (" Hours   : %d\n", date->hours);
+	printf (" Minutes : %d\n", date->minutes);
+	printf (" Seconds : %f\n", date->seconds);
+}
+
+int main (int argc, char * argv[])
+{
+	struct ln_equ_posn equ;
+	struct ln_rst_time rst;
+	struct ln_date rise, set, transit;
+	struct ln_lnlat_posn observer;
+	struct ln_helio_posn pos;
+	double JD;
+	
+	/* observers location (Edinburgh), used to calc rst */
+	observer.lat = 55.92;
+	observer.lng = 3.18;
+	
+	/* get Julian day from local time */
+	JD = get_julian_from_sys();	
+	printf ("JD %f\n", JD);
+	
+	/* geometric coordinates */
+	get_geom_solar_coords (JD, &pos);
+	printf("Solar Coords longitude (deg) %f\n", pos.L);
+	printf("             latitude (deg) %f\n", pos.B);
+	printf("             radius vector (AU) %f\n", pos.R);
+	
+	/* ra, dec */
+	get_equ_solar_coords (JD, &equ);
+	printf("Solar Position RA %f\n", equ.ra);
+	printf("               DEC %f\n", equ.dec);
+	
+	/* rise, set and transit */
+	if (get_solar_rst (JD, &observer, &rst) == 1) 
+		printf ("Comet is circumpolar\n");
+	else {
+		get_local_date (rst.rise, &rise);
+		get_local_date (rst.transit, &transit);
+		get_local_date (rst.set, &set);
+		print_date ("Rise", &rise);
+		print_date ("Transit", &transit);
+		print_date ("Set", &set);
+	}
+	
+	return 0;
+}
