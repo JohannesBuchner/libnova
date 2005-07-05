fae59d4044cb194c42aa144ef4e1b30f55bebce7
l_girdwood <l_girdwood@a9329980-0c59-416b-a420-72dd107c4633>
2005-07-05 09:16:06 +0000

    updated copyright and LGPL

--- src/misc.c
+++ src/misc.c
@@ -1,22 +1,23 @@
-/* $Id: misc.c,v 1.7 2003-08-25 20:42:47 l_girdwood Exp $
-**
-* Copyright (C) 1999, 2000 Juan Carlos Remis
-* Copyright (C) 2002 Liam Girdwood
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU Library General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
+/* $Id: misc.c,v 1.8 2005-07-05 09:16:06 l_girdwood Exp $
+ **
+ * Copyright (C) 1999, 2000 Juan Carlos Remis
+ * Copyright (C) 2002 Liam Girdwood
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *     
+ */
 
 /*------------------------------------------------------------------------*/
 /*                                                                        */
--- src/neptune.c
+++ src/neptune.c
@@ -1,25 +1,23 @@
 /*
- 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU Library General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
-
-Some functions in this file use the VSOP87 solution by
-Messrs. Bretagnon and Francou.
-
-Copyright 2000 Liam Girdwood
- 
-*/
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * 
+ *  Some functions in this file use the VSOP87 solution by
+ *  Messrs. Bretagnon and Francou.
+ * 
+ *  Copyright (C) 2000 - 2005 Liam Girdwood  
+ */
 
 #include <math.h>
 #include <libnova/neptune.h>
