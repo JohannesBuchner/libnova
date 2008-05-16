4b238c4eea1b287b2d52331f87a6524b59ab704d
pkubanek <pkubanek@a9329980-0c59-416b-a420-72dd107c4633>
2008-05-16 12:50:20 +0000

    update version to 0.12.2

--- src/libnova/libnova.h
+++ src/libnova/libnova.h
@@ -58,7 +58,7 @@
 * API documentation for libnova is included in the source. It can also be found in this website and an offline tarball is available <A href="http://libnova.sf.net/libnovadocs.tar.gz">here</A>.
 *
 * \section download Download
-* The latest released version of libnova is 0.11.
+* The latest released version of libnova is 0.12.2.
 * It is available for download <A href="http://sf.net/project/showfiles.php?group_id=57697">here.</A>
 *
 * \section cvs CVS
